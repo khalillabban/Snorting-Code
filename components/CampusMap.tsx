520c61f5ed82fd9427cf36821b85db562b857cec
khalillabban <khalil.labban@yahoo.com>
2026-01-31 18:02:34 -0500

    WIP - Created toggle button for campuses

--- components/CampusMap.tsx
+++ components/CampusMap.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { StyleSheet, View } from "react-native";
 import MapView from "react-native-maps";
 
@@ -6,19 +7,31 @@ type Coordinates = {
   longitude: number;
 };
 
-export default function CampusMap({ coordinates }: { coordinates: Coordinates }) {
+export default function CampusMap({
+  coordinates,
+}: {
+  coordinates: Coordinates;
+}) {
+  const mapRef = useRef<MapView>(null);
+
+  useEffect(() => {
+    mapRef.current?.animateToRegion(
+      {
+        latitude: coordinates.latitude,
+        longitude: coordinates.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      },
+      250 // ms
+    );
+  }, [coordinates]);
+
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         style={StyleSheet.absoluteFillObject}
-        initialRegion={{
-          latitude: coordinates.latitude,
-          longitude: coordinates.longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
         showsUserLocation
-        showsMyLocationButton
       />
     </View>
   );
