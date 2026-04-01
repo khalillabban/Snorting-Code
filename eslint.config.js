d4566869c1389c4e3da8d409eec76bccaf7d85fb
Nicholas Gallacher <nrgallacher99@gmail.com>
2026-04-01 14:36:57 -0400

    declare jest globals in ESLint flat config for test files

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,19 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const globals = require('globals');
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+  },
+  {
+    files: ['jest/**/*.js', '__tests__/**/*.{ts,tsx}', '**/*.test.{ts,tsx}', 'jest.setup.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
   },
 ]);
